eaabc36cc9a2b684c51256fcc9286d25bb8ccd12
ray1888 <alal1995@hotmail.com>
2022-09-14 22:11:59 +0800

    feat(IF):  add if impl

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -8,6 +8,11 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a < b {
+        return b;
+    } else {
+        return a;
+    }
 }
 
 // Don't mind this for now :)
