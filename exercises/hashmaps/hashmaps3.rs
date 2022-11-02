749c6fa2ce767eb3cf7ac7f67b75d3b2db115d44
ray1888 <alal1995@hotmail.com>
2022-11-02 06:51:15 +0000

    feat(hashmaps): add hashmaps feature

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -35,11 +35,28 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        scores
+            .entry(team_1_name.clone())
+            .and_modify(|e| {
+                e.goals_scored += team_1_score;
+                e.goals_conceded += team_2_score
+            })
+            .or_insert(Team {
+                name: team_1_name.clone(),
+                goals_scored: team_1_score.clone(),
+                goals_conceded: team_2_score.clone(),
+            });
+        scores
+            .entry(team_2_name.clone())
+            .and_modify(|e| {
+                e.goals_scored += team_2_score;
+                e.goals_conceded += team_1_score
+            })
+            .or_insert(Team {
+                name: team_2_name.clone(),
+                goals_scored: team_2_score.clone(),
+                goals_conceded: team_1_score.clone(),
+            });
     }
     scores
 }
